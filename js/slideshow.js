b553318ca8cb3ca8656bfcaccdc918133603377d
monejone <37658604+monejone@users.noreply.github.com>
2018-03-22 15:44:37 +0000

    changes

---
+++ js/slideshow.js
@@ -0,0 +1,26 @@
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var content = document.getElementsByClassName("myContent");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+	  content[i].style.display= "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+  content[slideIndex-1].style.display = "block"; 
+}
